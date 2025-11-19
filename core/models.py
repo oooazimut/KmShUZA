43e19f626f78e8d4674bc538c97a9095c7a2d56a
Marat <kitvideo-pc3@ya.ru>
2025-11-19 21:27:08 +0300

    second commit

--- core/models.py
+++ core/models.py
@@ -1,13 +1,22 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 
-@dataclass
+@dataclass(frozen=True)
 class Pump:
-    is_working: bool
-    pressure: float
-    work: int
+    id: int
+    is_working: bool = False
+    pressure: float = 0
+    runtime_minutes: int = 0
+    timestamp: datetime = datetime.today()
+
+    @property
+    def runtime_hours(self):
+        return int(self.runtime_minutes / 60)
 
 
-@dataclass
+@dataclass(frozen=True)
 class Uza:
     is_active: bool
+    selector: int
+    permission: bool
--- core/ports.py
+++ core/ports.py
@@ -1,11 +1,14 @@
-from typing import Protocol
+from datetime import date
+from typing import Iterable, Protocol
+
+from core.models import Pump, Uza
 
 
 class Receiver(Protocol):
-    def get_data(self):
-        pass
+    def receive_data(self) -> Iterable[Pump | Uza | None]: ...
 
 
 class Repo(Protocol):
-    def save_data(self):
-        pass
+    def save_data(self, data: Iterable[Pump | Uza]): ...
+
+    def get_pumps_by_date(self, date: date) -> Iterable[Pump | None]: ...
