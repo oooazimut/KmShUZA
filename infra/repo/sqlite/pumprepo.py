b550848e7142dc283145b80e134e40ca65ddb512
Marat <kitvideo-pc3@ya.ru>
2025-11-23 21:24:44 +0300

    пилю ресивер

--- infra/repo/sqlite/pumprepo.py
+++ infra/repo/sqlite/pumprepo.py
@@ -1,8 +1,10 @@
-import sqlite3 as sq
-from contextlib import contextmanager
+import asyncio
+from contextlib import asynccontextmanager
 from datetime import date
 from pathlib import Path
 from typing import Iterable
+
+import aiosqlite as sq
 
 from domain.models import Pump
 
@@ -10,50 +12,56 @@ from .mapping import from_row, to_row
 
 
 class SqlitePumpRepo:
-    def __init__(self, db_path: str) -> None:
-        self.db_path = Path(db_path)
+    def __init__(self, db_path: str, init_script: str) -> None:
+        self._db_path = Path(db_path)
+        self._init_script: Path = Path(init_script)
+        self._conn: sq.Connection | None = None
+        self._lock = asyncio.Lock()
 
-    @contextmanager
-    def _transaction(self):
-        conn = sq.connect(
-            self.db_path, detect_types=sq.PARSE_DECLTYPES | sq.PARSE_COLNAMES
-        )
-        conn.row_factory = sq.Row
-        try:
-            yield conn
-            conn.commit()
-        except Exception:
-            conn.rollback()
-            raise
-        finally:
-            conn.close()
+    async def connect(self):
+        self._conn = await sq.connect(self._db_path)
+        self._conn.row_factory = sq.Row
 
-    def create_db(self, db_name: str, script_name: Path):
-        with open(script_name, "r", encoding="utf-8") as file:
+    async def close_conn(self):
+        if self._conn is not None:
+            await self._conn.close()
+            self._conn = None
+
+    @asynccontextmanager
+    async def _transaction(self):
+        async with self._lock:
+            try:
+                await self._conn.execute("BEGIN")
+                yield self._conn
+                await self._conn.commit()
+            except:
+                await self._conn.rollback()
+                raise
+
+    async def create_db(self):
+        with open(self._init_script, "r", encoding="utf-8") as file:
             script = file.read()
 
-        with self._transaction() as conn:
-            conn.executescript(script)
+        async with self._transaction() as conn:
+            await conn.executescript(script)
 
-    def save_pumps(self, pumps: Iterable[Pump]) -> Iterable[Pump]:
+    async def save_pumps(self, pumps: Iterable[Pump]) -> Iterable[Pump]:
         saved_pumps = []
         query = """INSERT INTO pumps (name, is_working, pressure, runtime_minutes)
                    VALUES (:name, :is_working, :pressure, :runtime_minutes)
                    RETURNING *
                 """
-        with self._transaction() as conn:
+        async with self._transaction() as conn:
             for pump in pumps:
-                saved_pump = conn.execute(query, to_row(pump)).fetchone()
-                saved_pumps.append(from_row(saved_pump))
+                cursor = await conn.execute(query, to_row(pump))
+                saved_pumps.append(from_row(await cursor.fetchone()))
+
         return saved_pumps
 
-    def get_pumps_by_date(self, date: date) -> Iterable[Pump | None]:
+    async def get_pumps_by_date(self, date: date) -> Iterable[Pump | None]:
         query = "SELECT * FROM pumps WHERE DATE(timestamp) = ?"
-        with self._transaction() as conn:
-            result = conn.execute(
-                query,
-                [
-                    date.isoformat(),
-                ],
-            ).fetchall()
-        return [from_row(row) for row in result]
+        async with self._transaction() as conn:
+            cursor = await conn.execute(query, [date.isoformat()])
+            rows = await cursor.fetchall()
+
+        return [from_row(row) for row in rows]
